74f91e6900c130f03c573c1f57e15661fa7e1315
Kevin Vaysse <vayssek@gmail.com>
2017-09-13 16:36:15 +0200

    ADD JPAConfiguration Class
    Adding import in Application class and Creation of the corresponding class

--- src/main/java/com/example/springboot/SpringBootCRUDApplication/SpringBootCrudApplication.java
+++ src/main/java/com/example/springboot/SpringBootCRUDApplication/SpringBootCrudApplication.java
@@ -2,7 +2,11 @@ package com.example.springboot.SpringBootCRUDApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
+import com.example.springboot.SpringBootCRUDApplication.configuration.JpaConfiguration;
+
+@Import(JpaConfiguration.class)
 @SpringBootApplication
 public class SpringBootCrudApplication {
 
